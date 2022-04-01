299b37528fc7da321a8242b6e64d69ded5b24990
Mdd <jokejokepasjoke>
2022-04-01 13:58:59 -0400

    initial commit

---
+++ src/main/java/mdd/solutions/MonoSound/MonoSoundApplication.java
@@ -0,0 +1,13 @@
+package mdd.solutions.MonoSound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoSoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoSoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mdd/solutions/MonoSound/MonoSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package mdd.solutions.MonoSound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoSoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
